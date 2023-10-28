cbdb006989671a6e03ecedd227b07b7d3786888e
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-10-28 19:38:52 +0200

    changes spacing

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -7,18 +7,20 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import logo from '../logo.png';
 import '../styles/NavigationBar.scss';
-
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 function NavigationBar() {
   return (
-    <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+    <Navbar expand="sm" bg="dark" data-bs-theme="dark">
       <Container fluid>
         <Navbar.Brand href="#home"><img src={logo} alt="Logo" style={{ height: '30px' }}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
-            <Nav.Item><Button variant="danger">HOME</Button></Nav.Item>
+            <Nav.Item><Button variant="danger" id='home-btn'>HOME</Button></Nav.Item>
             <Nav.Item>
               <Dropdown as={ButtonGroup}>
                 <Button variant="danger">NEWS</Button>
@@ -69,6 +71,20 @@ function NavigationBar() {
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
+        <Form>
+          <Row>
+            <Col xs="auto">
+              <Form.Control
+                type="text"
+                placeholder="Cerca"
+                className=" mr-sm-2"
+              />
+            </Col>
+            <Col xs="auto">
+              <Button type="submit" variant='danger'>Submit</Button>
+            </Col>
+          </Row>
+        </Form>
       </Container>
     </Navbar>
   );

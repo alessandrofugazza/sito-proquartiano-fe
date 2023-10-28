9c513f8bb08bf211834ddb5fc8b45ea5853533b0
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-10-29 00:34:57 +0200

    adds mt-auto to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import "../styles/Footer.scss"
 
 function Footer() {
     return(
-        <footer>
+        <footer className='mt-auto'>
             <div className="py-3">
                 <Container   >
                     <p className='my-0'>

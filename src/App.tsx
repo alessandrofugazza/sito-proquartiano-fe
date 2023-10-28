4690caad40074c635e4c03eab4f19b526d838bd4
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-10-28 19:50:32 +0200

    adds routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import NavigationBar from './components/NavigationBar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
+      <BrowserRouter>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="*"
+            element={<NotFound />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

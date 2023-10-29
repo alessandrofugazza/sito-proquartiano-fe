59c6a2140a2c61fd79d036e53bc64810829c5311
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-10-29 02:44:31 +0100

    fixes year archive component

--- src/components/news/YearArchive.tsx
+++ src/components/news/YearArchive.tsx
@@ -7,21 +7,36 @@ interface YearArchiveProps {
 
 
 function YearArchive({year}: YearArchiveProps) {
-  const months: string[] = [
+
+  let months: string[] = [
     'Gennaio', 'Febbraio', 'Marzo', 'Aprile',
     'Maggio', 'Giugno', 'Luglio', 'Agosto',
     'Settembre', 'Ottobre', 'Novembre', 'Dicembre'
   ];
-  const monthsNumber: string[] = [
+
+  let monthsNumber: string[] = [
     '01', '02', '03', '04',
     '05', '06', '07', '08',
     '09', '10', '11', '12'
   ];
+
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear().toString();
+  const currentMonthIndex = currentDate.getMonth();
+
+  if (year === currentYear) {
+    months = months.slice(0, currentMonthIndex + 1).reverse();
+    monthsNumber = monthsNumber.slice(0, currentMonthIndex + 1).reverse();
+  } else {
+    months.reverse();
+    monthsNumber.reverse();
+  }
+
   return(
     <ListGroup >
-      {monthsNumber.reverse().map((month, index) => (
-        <Link to={`news/${year}/${month}`} key={month} style={{textDecoration: "none"}}>
-          <ListGroup.Item>{months.reverse()[index]} {year}</ListGroup.Item>
+      {months.map((month, index) => (
+        <Link to={`news/${year}/${monthsNumber[index]}`} key={monthsNumber[index]} style={{textDecoration: "none"}}>
+          <ListGroup.Item>{month} {year}</ListGroup.Item>
         </Link>
       ))}
     </ListGroup >

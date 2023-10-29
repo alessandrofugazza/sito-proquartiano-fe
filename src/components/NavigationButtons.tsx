b2c68b7f692d30c7429e90a854cc79b4267e7eb0
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-10-29 20:01:01 +0100

    updates navigation buttons layout

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -12,14 +12,14 @@ function NavigationButtons() {
   sessionStorage.setItem("previousUrl", useLocation().pathname)
 
   return(
-    <>
+    <div className="d-flex justify-content-end gap-4">
       {previousUrl && <Link to={previousUrl}>
         <Button variant="danger">Torna indietro</Button>
       </Link>}
       <Link to="/">
         <Button variant="danger">Torna alla pagina principale</Button>
       </Link>
-    </>
+    </div>
   )
 }
 

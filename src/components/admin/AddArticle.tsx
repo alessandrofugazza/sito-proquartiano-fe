dd53f99ae927e754a2f027dec34629f0a7457de6
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-11-07 02:54:59 +0100

    updates layout

--- src/components/admin/AddArticle.tsx
+++ src/components/admin/AddArticle.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { CloseButton, InputGroup } from "react-bootstrap";
+import { CloseButton, Col, InputGroup, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
@@ -35,49 +35,63 @@ export default function AddArticle() {
   };
 
   return (
-    <>
-      <Form onSubmit={handleFormSubmit}>
-        {/* <Form.Group className="mb-3" controlId="title">
-          <Form.Label>Titolo</Form.Label>
-          <Form.Control type="text" placeholder="Inserisci un titolo" />
-        </Form.Group> */}
-        <InputGroup className="mb-3">
-          <InputGroup.Text id="title">Titolo</InputGroup.Text>
-          <Form.Control placeholder="Inserisci un titolo" type="text" aria-label="Titolo" aria-describedby="title" />
-        </InputGroup>
-        <Form.Group className="mb-3 d-flex flex-column" controlId="title">
-          <Form.Label>Data e ora</Form.Label>
-          <input type="datetime-local" value={selectedDate} onChange={handleDateChange} />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="categories">
-          <Form.Label>Categorie</Form.Label>
-          <Form.Check type="checkbox" id="associazione" label="Associazione" />
-          <Form.Check type="checkbox" label="Concorso cori" id="concorso-cori" />
-          <Form.Check type="checkbox" label="Manifestazioni" id="manifestazioni" />
-          <Form.Check type="checkbox" label="Rassegna stampa" id="rassegna-stampa" />
-        </Form.Group>
-      </Form>
-      <Form onSubmit={addNewTag}>
-        <Form.Group className="mb-3" controlId="tags">
-          <Form.Label>Tags</Form.Label>
-          <Form.Control
-            placeholder="Inserisci un nuovo tag"
-            type="text"
-            aria-label="Tag"
-            aria-describedby="tag"
-            value={newTag}
-            onChange={handleNewTagChange}
-          />
-        </Form.Group>
-      </Form>
-      <div>
-        {tags.map(tag => (
-          <Button as="div" variant="light" className="border border-dark" size="sm" key={tag}>
-            {tag}
-            <CloseButton onClick={e => handleRemoveTag(tag, e)} />
-          </Button>
-        ))}
-      </div>
-    </>
+    <Row>
+      <Col lg="6">
+        <Form onSubmit={handleFormSubmit} className="">
+          {/* <Form.Group className="mb-3" controlId="title">
+            <Form.Label>Titolo</Form.Label>
+            <Form.Control type="text" placeholder="Inserisci un titolo" />
+          </Form.Group> */}
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="title">Titolo</InputGroup.Text>
+            <Form.Control placeholder="Inserisci un titolo" type="text" aria-label="Titolo" aria-describedby="title" />
+          </InputGroup>
+          <Form.Group className="mb-3 d-flex flex-column" controlId="title">
+            <Form.Label>Data e ora</Form.Label>
+            <input type="datetime-local" value={selectedDate} onChange={handleDateChange} />
+          </Form.Group>
+        </Form>
+      </Col>
+      <Col lg="3">
+        <Form>
+          <Form.Group className="mb-3" controlId="categories">
+            <Form.Label>Categorie</Form.Label>
+            <Form.Check type="checkbox" id="associazione" label="Associazione" />
+            <Form.Check type="checkbox" label="Concorso cori" id="concorso-cori" />
+            <Form.Check type="checkbox" label="Manifestazioni" id="manifestazioni" />
+            <Form.Check type="checkbox" label="Rassegna stampa" id="rassegna-stampa" />
+          </Form.Group>
+        </Form>
+      </Col>
+      <Col lg="3">
+        <Form onSubmit={addNewTag}>
+          <Form.Group className="mb-3" controlId="tags">
+            <Form.Label>Tags</Form.Label>
+            <Form.Control
+              placeholder="Inserisci un nuovo tag"
+              type="text"
+              aria-label="Tag"
+              aria-describedby="tag"
+              value={newTag}
+              onChange={handleNewTagChange}
+            />
+          </Form.Group>
+          <div className="d-flex gap-2 flex-wrap">
+            {tags.map(tag => (
+              <Button
+                as="div"
+                variant="light"
+                className="border border-dark d-flex align-items-center"
+                size="sm"
+                key={tag}
+              >
+                {tag}
+                <CloseButton onClick={e => handleRemoveTag(tag, e)} />
+              </Button>
+            ))}
+          </div>
+        </Form>
+      </Col>
+    </Row>
   );
 }

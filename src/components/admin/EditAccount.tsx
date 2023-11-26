b8a94716a9eaf625380612499517dd5184b854dd
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-11-26 02:40:15 +0100

    edit profile draft

--- src/components/admin/EditAccount.tsx
+++ src/components/admin/EditAccount.tsx
@@ -1,16 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+interface IProfileData {
+  email: string;
+  signature: string;
+}
 
 export default function EditAccount() {
+  const [profileData, setProfileData] = useState<IProfileData | null>(null);
   const fetchProfileData = async () => {
     const re = await fetch("http://localhost:3001/admins/profilo", {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("loginToken")}`,
       },
     });
-    console.log(await re.json());
+    const profileData = await re.json();
+    setProfileData(profileData);
   };
   useEffect(() => {
     fetchProfileData();
   }, []);
-  return <></>;
+  return (
+    <>
+      <div className="d-flex justify-content-between">
+        <span>Email:</span>
+        <span>{profileData?.email}</span>
+      </div>
+      <div className="d-flex justify-content-between">
+        <span>Username:</span>
+        <span>{profileData?.signature}</span>
+      </div>
+    </>
+  );
 }

4cf243a7c87570d934ef71f0640da131ea1ec884
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-12-08 15:42:03 +0100

    fixed bug in navigation buttons

--- src/components/NavigationButtons.tsx
+++ src/components/NavigationButtons.tsx
@@ -1,20 +1,28 @@
 import { Button } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/NavigationButtons.scss";
+import { useEffect } from "react";
 
 function NavigationButtons() {
   // ? is this really the smartest way to do this?
-  const previousUrl = sessionStorage.getItem("previousUrl");
-  if (!previousUrl) {
-    sessionStorage.setItem("previousUrl", window.location.href);
-  }
+  // const previousUrl = sessionStorage.getItem("previousUrl");
+  // if (!previousUrl) {
+  //   sessionStorage.setItem("previousUrl", window.location.href);
+  // }
+  // ? did i break something?
 
   const navigate = useNavigate();
 
   const handleGoBack = () => {
     navigate(-1);
   };
-
+  const currentUrl = window.location.href;
+  const previousUrl = sessionStorage.getItem("previousUrl");
+  useEffect(() => {
+    if (previousUrl !== currentUrl) {
+      sessionStorage.setItem("previousUrl", currentUrl);
+    }
+  }, []);
   return (
     <div className="d-flex justify-content-center justify-content-md-end gap-4 mt-auto ">
       {previousUrl && (

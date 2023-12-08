cfaeaadb8da6193252772e0ed12456eb61b9a07c
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-12-08 14:37:46 +0100

    adds social to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,34 @@ function Footer() {
   return (
     <footer>
       <div className="py-3">
-        <Container>
-          <p className="my-0">
-            Via IV Novembre 65
-            <br />
-            26837 - Quartiano di Mulazzano (LO)
-            <br />
-            Italia
-          </p>
+        <Container className="d-flex justify-content-between">
+          <div>
+            <p className="my-0">
+              Via IV Novembre 65
+              <br />
+              26837 - Quartiano di Mulazzano (LO)
+              <br />
+              Italia
+            </p>
+          </div>
+          <div className="d-flex gap-3">
+            <a
+              href="https://m.facebook.com/profile.php/?id=100090483283325"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "inherit" }}
+            >
+              <i className="bi bi-facebook fs-4 text-align-center"></i>
+            </a>
+            <a
+              href="https://www.youtube.com/@proquartiano"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "inherit" }}
+            >
+              <i className="bi bi-youtube  fs-4 text-align-center"></i>
+            </a>
+          </div>
         </Container>
       </div>
       <div className="py-3">
--- src/components/admin/PublishedArticles.tsx
+++ src/components/admin/PublishedArticles.tsx
@@ -1,3 +1,17 @@
+import { useEffect } from "react";
+
 export default function PublishedArticles() {
+  const fetchArticles = async () => {
+    const re = await fetch(`${process.env.REACT_APP_API_URL}/articoli/my-articles`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("loginToken")}`,
+      },
+    });
+    console.log(await re.json());
+  };
+  useEffect(() => {
+    fetchArticles();
+  }, []);
   return <></>;
 }
